543312232cf349c6ac5b64e00944050a7ca2d046
mlengse <anjangkn@gmail.com>
2020-02-08 08:16:56 +0700

    back

--- runner.js
+++ runner.js
@@ -9,21 +9,21 @@ const ekin = new Core(config)
 module.exports = async () => {
   try {
     await ekin.init()
-    for(let a of ekin.nums) {//if( a == 0 || (a == -1 && ekin.tglSkrg < 5 )) {
-      if(a === -1) {
-        for( let i in ekin.users) {
-          await ekin.login( ekin.users[i] )
-        // if( a === 0 && (i === 'anjang' || i === 'monic')) {
-        //   await ekin.getKdSKP()
-        //   await ekin.getKegTahun()
-        //   await ekin.getKegBulan({ a })
-        //   await ekin.fetchRealKeg({ a })
-        //   await ekin.inputHarian({ a, i })
-        // }
-        // if( a == 0 || ( a == -1 
-        //   && (( ekin.tglSkrg < 7 ) && ( i === 'yuni' || i === 'anjang' || i === 'wagimin')) 
-        //   || (( ekin.tglSkrg < 3 ) && ( i !== 'yuni' && i !== 'anjang' && i !== 'wagimin'))
-        //   )) {
+    for(let a of ekin.nums) if( a == 0 || (a == -1 && ekin.tglSkrg < 5 )) {
+      // if(a === -1) {
+      for( let i in ekin.users) {
+        await ekin.login( ekin.users[i] )
+        if( a === 0 && (i === 'anjang' || i === 'monic')) {
+          await ekin.getKdSKP()
+          await ekin.getKegTahun()
+          await ekin.getKegBulan({ a })
+          await ekin.fetchRealKeg({ a })
+          await ekin.inputHarian({ a, i })
+        }
+        if( a == 0 || ( a == -1 
+          && (( ekin.tglSkrg < 7 ) && ( i === 'yuni' || i === 'anjang' || i === 'wagimin')) 
+          || (( ekin.tglSkrg < 3 ) && ( i !== 'yuni' && i !== 'anjang' && i !== 'wagimin'))
+          )) {
           await ekin.getDataBawahan()
           await ekin.getLaporanTamsil({ a })
           await ekin.approveKegStaff( { a, i })
