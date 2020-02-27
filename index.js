0947665038e1b6430ecf438feccedc93b959c04c
mlengse <anjangkn@gmail.com>
2020-02-27 08:50:07 +0700

    sdfsf

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { schedule } = require('node-cron')
 const runner = require('./runner')
-schedule('10 15 * * *', async () => {
+schedule('43 8 * * *', async () => {
 	try{
 		await runner()
 	}catch(err){
