61d7470ba3c3147bc9b73c429a8add8dff090218
mlengse <anjangkn@gmail.com>
2020-05-18 13:30:09 +0700

    20200518

--- core/lib/pptr/functions/ekin/ekin_lib/get_user_login.js
+++ core/lib/pptr/functions/ekin/ekin_lib/get_user_login.js
@@ -4,7 +4,16 @@ exports._getUserLogin = async ({ that }) => {
   }
   if( that.isLogin) {
     that.spinner.start('get username')
-    let response = await that.page.evaluate( async () => document.getElementById('header').innerText.trim().split('\n').join('|').split('|').map(e => e.trim()).filter(e => e!=='Keluar'))
+    let response
+    while(!response){
+      response = await that.page.evaluate( async () => {
+        let doc = document.getElementById('header')
+        if(doc){
+          return doc.innerText.trim().split('\n').join('|').split('|').map(e => e.trim()).filter(e => e!=='Keluar')
+        }
+        return null
+      })
+    }
     that.user = {
       nl: response[0],
       username: response[1],
